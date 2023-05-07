97a57c37b3a637514ff3687758161d82cc74a071
Vladislav Demyanov <v.demyanov.dev@gmail.com>
2023-05-07 18:02:13 +0300

    Implement shelf in use check.

--- WMS.Frontend/src/app/dictionaries/addresses/models/shelf.ts
+++ WMS.Frontend/src/app/dictionaries/addresses/models/shelf.ts
@@ -1,9 +1,14 @@
+import { IWare } from 'src/app/wares/models/ware';
 import { IVerticalSection } from './vertical-section';
+import { IProblem } from 'src/app/problems/models/problem';
 
 export interface IShelf {
   Id: number;
   VerticalSectionId: number;
   Index: number;
+  InUse: boolean;
 
   VerticalSection?: IVerticalSection;
+  Ware?: IWare;
+  Problem?: IProblem;
 }
--- WMS.Frontend/src/app/dictionaries/addresses/racks/services/shelfs.service.ts
+++ WMS.Frontend/src/app/dictionaries/addresses/racks/services/shelfs.service.ts
@@ -14,12 +14,24 @@ export class ShelfsService {
   constructor(private readonly http: HttpClient) { }
 
   public getAllByVerticalSectionId(id: number): Observable<IShelf[]> {
-    return this.http.get<ODataValue<IShelf>>(`${ApiEndpoints.Shelfs}?$filter=VerticalSectionId eq ${id}`)
-      .pipe(map((odataValue: ODataValue<IShelf>) => odataValue.value));
+    const expandQuery: string = '$expand=Ware,Problem';
+    const filterQuery: string = `$filter=VerticalSectionId eq ${id}`;
+
+    return this.http.get<ODataValue<IShelf>>(`${ApiEndpoints.Shelfs}?${expandQuery}&${filterQuery}`)
+      .pipe(map((odataValue: ODataValue<IShelf>) => odataValue.value.map(x => this.populateInUseField(x))));
   }
 
   public getById(id: number): Observable<IShelf> {
-    const expandQuery: string = '$expand=VerticalSection($expand=Rack($expand=Area))';
-    return this.http.get<IShelf>(`${ApiEndpoints.Shelfs}${id}?${expandQuery}`);
+    const expandQuery: string = '$expand=Ware,Problem,VerticalSection($expand=Rack($expand=Area))';
+    return this.http.get<IShelf>(`${ApiEndpoints.Shelfs}${id}?${expandQuery}`)
+      .pipe(map(shelf => this.populateInUseField(shelf)));
+  }
+
+  private populateInUseField(shelf: IShelf): IShelf {
+    if (shelf.Problem || shelf.Ware) {
+      shelf.InUse = true;
+    }
+
+    return shelf;
   }
 }
