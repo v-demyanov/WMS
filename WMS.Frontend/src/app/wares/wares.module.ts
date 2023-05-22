9a8dbfbb49309a507bb1099548644bcb1e906fec
Vladislav Demyanov <v.demyanov.dev@gmail.com>
2023-05-22 03:52:15 +0300

    Update documentation.

--- WMS.Frontend/src/app/wares/wares.module.ts
+++ WMS.Frontend/src/app/wares/wares.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { WaresComponent } from './wares.component';
 import { WaresRoutingModule } from './wares-routing.module';
 import { WaresNavigationComponent } from './wares-navigation/wares-navigation/wares-navigation.component';
@@ -8,7 +9,6 @@ import { SharedModule } from '../shared/shared.module';
 import { WaresToolbarComponent } from './wares-toolbar/wares-toolbar/wares-toolbar.component';
 import { WareRestoreDialogComponent } from './ware-restore-dialog/ware-restore-dialog.component';
 import { WaresFilterDialogComponent } from './wares-filter-dialog/wares-filter-dialog.component';
-
 
 @NgModule({
   declarations: [
